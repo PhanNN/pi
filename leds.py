65bbcb08b34815ee6fc181949e1a0ebadfe80a50
Pi <nguyengocphan@gmail.com>
2018-03-12 21:12:30 +0700

    _:init proj with leds.py demo

---
+++ leds.py
@@ -0,0 +1,82 @@
+# Using PWM with RPi.GPIO
+
+import RPi.GPIO as GPIO
+import time
+import sys
+from random import randint
+import time
+# from pubnub import Pubnub
+
+GPIO.setmode(GPIO.BCM)
+
+PIN_LIVING = 23
+PIN_PORCH = 17
+PIN_FIREPLACE = 27
+
+GPIO.setup(PIN_LIVING,GPIO.OUT)
+GPIO.setup(PIN_PORCH,GPIO.OUT)
+GPIO.setup(PIN_FIREPLACE,GPIO.OUT)
+
+FREQ = 100 # frequency in Hz
+FIRE_FREQ = 30 #  flickering effect
+
+# Duty Cycle (0 <= dc <=100)
+
+living = GPIO.PWM(PIN_LIVING, FREQ)
+living.start(0)
+
+porch = GPIO.PWM(PIN_PORCH, FREQ)
+porch.start(0)
+
+fire = GPIO.PWM(PIN_FIREPLACE, FIRE_FREQ)
+fire.start(0)
+
+# PubNub
+
+# pubnub = Pubnub(publish_key='demo', subscribe_key='demo')
+
+# channel = 'pi-house'
+
+def _callback(m, channel):
+    print(m)
+
+    dc = m['brightness'] * 10
+
+    if m['item'] == 'light-living':
+         living.ChangeDutyCycle(dc)
+
+    elif m['item'] == 'light-porch':
+        porch.ChangeDutyCycle(dc)
+
+    elif m['item'] == 'fireplace':
+        fire.ChangeDutyCycle(dc)
+
+def _error(m):
+  print(m)
+
+# pubnub.subscribe(channels=channel, callback=_callback, error=_error)
+
+def _offAll():
+    living.ChangeDutyCycle(0)
+    porch.ChangeDutyCycle(0)
+    fire.ChangeDutyCycle(0)
+
+try:
+    while 1:
+        led = randint(0,2)
+#       randDc = randint(30,100)
+        _offAll()
+        if led == 0:
+            living.ChangeDutyCycle(100)
+#            print 'green'
+        elif led == 1:
+            porch.ChangeDutyCycle(100)
+#            print 'yellow'
+        else:
+            fire.ChangeDutyCycle(100)
+#            print 'red'
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    sys.exit(1)
